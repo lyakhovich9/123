03cb08516f93ba8b002439f3f4f9e8c713c07495
lyakhovich <lizenka37@gmail.com>
2024-05-05 22:00:53 +0300

    проверка пароля

---
+++ models/UserRegister.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace app\models;
+
+class UserRegister extends User {
+    public $password_confirmation = '';
+
+    public function rules() {
+        return array_merge(
+
+           parent::rules(),
+           [
+            ['password_confirmation', 'compare', 'compareAttribute'=>'password', 'message'=>'Пароли не совпадают'],
+           ]
+        );
+    }
+}
