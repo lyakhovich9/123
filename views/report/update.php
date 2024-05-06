e2303e0c06ec4463b6280eb5370873cf5339ace3
lyakhovich <lizenka37@gmail.com>
2024-05-06 12:57:15 +0300

    Страница обноваления заявления

--- views/report/update.php
+++ views/report/update.php
@@ -5,16 +5,15 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\Report $model */
 
-$this->title = 'Update Report: ' . $model->id;
-$this->params['breadcrumbs'][] = ['label' => 'Reports', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
+$this->title = 'Обновление статуса заявления: ' . $model->id;
+$this->params['breadcrumbs'][] = ['label' => 'Заявления', 'url' => ['index']];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="report-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
+    <?= $this->render('_form_admin', [
         'model' => $model,
     ]) ?>
 
