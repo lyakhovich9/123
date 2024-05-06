dd79e9eb2c3564eb1ec13ad4588e3712253fec32
lyakhovich <lizenka37@gmail.com>
2024-05-06 12:56:43 +0300

    Страница заявлений для админа

---
+++ views/report/index_admin.php
@@ -0,0 +1,48 @@
+<?php
+
+use app\models\Report;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/** @var yii\web\View $this */
+/** @var app\models\ReportSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Заявления';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="report-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'summary'=>'',
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'user',
+            'number',
+            'description:ntext',
+            
+            'status',
+            [
+                'template' => '{update}',
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Report $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
